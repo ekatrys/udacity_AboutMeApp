8a5e0069869ca210241fd30ceeba300b9df5e73a
Ekaterina <ekatrys@gmail.com>
2020-09-22 11:31:24 +0200

    add findById

--- app/src/main/java/com/example/myapplication/MainActivity.kt
+++ app/src/main/java/com/example/myapplication/MainActivity.kt
@@ -1,35 +1,32 @@
 package com.example.myapplication
 
 import android.content.Context
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.View
 import android.view.inputmethod.InputMethodManager
-import androidx.databinding.DataBindingUtil
-import com.example.myapplication.databinding.ActivityMainBinding
-import kotlinx.android.synthetic.main.activity_main.*
+import android.widget.Button
+import android.widget.TextView
+import androidx.appcompat.app.AppCompatActivity
+
 
 class MainActivity : AppCompatActivity() {
 
-    private lateinit var binding: ActivityMainBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        binding = DataBindingUtil.setContentView(this, R.layout.activity_main)
+        setContentView(R.layout.activity_main)
 
-        binding.doneButton.setOnClickListener {
-            addNickname(it)
+        val doneButton = findViewById<Button>(R.id.done_button)
+        val nicknameText = findViewById<TextView>(R.id.nickname_text)
+        val nicknameEddit = findViewById<TextView>(R.id.nickname_eddit)
+
+        doneButton.setOnClickListener {
+            nicknameText.text = nicknameEddit.text
+            nicknameEddit.visibility = View.GONE
+            doneButton.visibility = View.GONE
+            nicknameText.visibility = View.VISIBLE
+
+            val imm = getSystemService(Context.INPUT_METHOD_SERVICE) as InputMethodManager
+            imm.hideSoftInputFromWindow(it.windowToken, 0)
         }
-    }
-
-    private fun addNickname(view: View) {
-        binding.apply {
-        nicknameText.text = nicknameEddit.text
-        nicknameEddit.visibility = View.GONE
-        doneButton.visibility = View.GONE
-        nicknameText.visibility = View.VISIBLE
-        }
-
-        val imm = getSystemService(Context.INPUT_METHOD_SERVICE) as InputMethodManager
-        imm.hideSoftInputFromWindow(view.windowToken, 0)
     }
 }
